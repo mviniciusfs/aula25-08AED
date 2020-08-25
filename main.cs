14bd1cf82b31254d1f5cf1a9e0c9acbc9fe1a2a2
Marcos_Vinici10 <viniciusfigueiredo@ucl.br>
2020-08-25 23:21:06 +0000

    v2

--- main.cs
+++ main.cs
@@ -3,34 +3,34 @@ using System;
 class MainClass {
   public static char CalculaConceito(double n1, double n2)
   {
+    
     double media = (n1 + n2)/2;
 
     if (media >= 9 && media <= 10)
     {
-      return 'A'
+      return 'A';
     }
 
     if (media >= 7.5 && media <= 9)
     {
-      return 'B'
+      return 'B';
     }
 
     if (media >= 6 && media <= 7.5)
     {
-      return 'C'
+      return 'C';
     }
 
     if (media >= 4 && media <= 6)
     {
-      return 'D'
+      return 'D';
     }
 
     if (media >= 0 && media <= 4)
     {
-      return 'E'
+      return 'E';
     }
 
-    
-    Console.WriteLine ("Hello World");
+    Console.WriteLine(CalculaConceito);    
   }
 }
